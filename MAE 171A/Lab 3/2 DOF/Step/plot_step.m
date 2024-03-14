a961f08ccbb6b40d41dd946bdf0f26b0e74a87fa
David H. Lim <davidlim4318@gmail.com>
2024-03-14 03:01:19 -0700

    redid control design
    transfer function from 1 dof step response results probably not accurate,
    
    manually inferred transfer function from bode plot, used to design controller

--- MAE 171A/Lab 3/2 DOF/Step/plot_step.m
+++ MAE 171A/Lab 3/2 DOF/Step/plot_step.m
@@ -1,29 +1,53 @@
 clear
 
-M = readmatrix("trial1.txt",'Whitespace',[';','[',']']);
-time = M(:,3);
-x1 = M(:,5);
-x2 = M(:,6);
-F = M(:,8);
-
 figure(1)
 clf
 hold on
 
-yyaxis left
-plot(time,x1,'b.','MarkerSize',10)
-plot(time,x2,'r.','MarkerSize',10)
-ylabel("Displacement (counts)")
-ax = gca;
-ax.YColor = 'k';
+for i = 1:3
 
-yyaxis right
-plot(time,F,'m.','MarkerSize',10)
-ylabel("Force (V)")
-ax = gca;
-ax.YColor = 'm';
+    M = readmatrix("2DOF_" + int2str(i),'Whitespace',[';','[',']']);
+    time = M(:,3);
+    x1 = M(:,5);
+    x2 = M(:,6);
+    F = M(:,7);
+    
+    yyaxis left
+    plot(time,x1,'b.','MarkerSize',10)
+    plot(time,x2,'r.','MarkerSize',10)
+    ylabel("Displacement (counts)")
+    ax = gca;
+    ax.YColor = 'k';
+    
+    yyaxis right
+    plot(time,F,'m.','MarkerSize',10)
+    ylabel("Force (V)")
+    ax = gca;
+    ax.YColor = 'm';
+   
+end
+
+%%
+k2 = 2.710e-3;
+m1 = 1.964e-6;
+d1 = 1.630e-5;
+m2 = 6.076e-6;
+d2 = 1.657e-6;
+
+syms t s
+%G = k2/(m1*m2*s^4 + (d1*m2 + d2*m1)*s^3 + (d1*d2 + k2*m1 + k2*m2)*s^2 + (d1*k2 + d2*k2)*s);
+%G = (200*s^3 + 3060*s^2 + 2700*s + 27000) / (s^4 + 0.24*s^3 +16*s^2);
+
+u = 0.5 - 0.5*heaviside(t-3);
+U = laplace(u);
+Y = G*U;
+y = ilaplace(Y);
+
+yyaxis left
+fplot(y,[0,6],'b-')
 
 hold off
+
 title("(a)")
 xlabel("Time (s)")
 legend('Disc 1','Disc 2','Control Effort','Location','best')
@@ -31,30 +55,3 @@ legend('Disc 1','Disc 2','Control Effort','Location','best')
 ax = gca;
 ax.TitleHorizontalAlignment = 'left';
 set(ax,'FontSize',12)
-
-%%
-k2 = 0.002710;
-m1 = 2.066e-6;
-d1 = 2.483e-5;
-m2 = 
-d2 = 
-
-syms t s
-G = k2/(m1*m2*s^4 + (d1*m2 + d2*m1)*s^3 + (d1*d2 + k2*m1 + k2*m2)*s^2 + (d1*k2 + d2*k2)*s);
-u = 0.5 - 0.5*heaviside(t-10);
-U = laplace(u);
-Y = G*U;
-y = ilaplace(Y);
-
-figure(2)
-clf
-hold on
-
-fplot(y,[0,20],'b-')
-
-M = readmatrix("trial2.txt",'Whitespace',[';','[',']']);
-time = M(:,3);
-x2 = M(:,6);
-plot(time,x1,'r--','MarkerSize',10)
-
-hold off
